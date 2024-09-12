f0eec3cc0aeb4670b35385fcc7a7b7dcba346d2c
DoDat3107 <169181171+DoDat3107@users.noreply.github.com>
2024-09-12 11:19:46 +0700

    Update test.sqla

--- test.sql
+++ test.sql
@@ -1,30 +1,32 @@
-
+DROP TABLE IF EXISTS listt;
 CREATE DATABASE store;
 USE store;
-
 CREATE TABLE listt (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(60) NOT NULL,
     nameSP VARCHAR(60) NOT NULL,
     price DOUBLE,
     ngaynhap VARCHAR(40) DEFAULT 'can phai nhap',
+    name VARCHAR(60) NOT NULL,
     gmail VARCHAR(100) UNIQUE
 );
 
-INSERT INTO listt (name, nameSP, price, ngaynhap, gmail) 
-VALUES  ('d', 'dat', 233, '31', 'example@gmail.com'),
-		('d', 'dat', 233, '31', 'example3@gmail.com'),	
-		('d1', 'dat2', 2332, '311', 'exampl2e@gmail.com');
 
-alter table listt add column sdt char(11) default "9123";
+INSERT INTO listt (nameSP, price, ngaynhap, name, gmail) 
+VALUES  ('dat', 233.5, '31', 'dd', 'example@gmail.com'),
+        ('dat', 233.4, '31', 'd', 'example3@gmail.com'),    
+        ('dat', 233, '31', 'd', 'example32@gmail.com'), 
+        ('dat2', 2332, '311', 'd1', 'exampl2e@gmail.com');
+
+ALTER TABLE listt ADD COLUMN sdt VARCHAR(11) DEFAULT '09123';
 ALTER TABLE listt DROP COLUMN gmail;
-update listt 
-set sdt = "928372"
-where id in (2);
 
-update listt 
-set sdt = "922232"
-where id in (3);
+UPDATE listt 
+SET sdt = '09283732'
+WHERE id = 2;
 
-
+UPDATE listt 
+SET sdt = '0922232'
+WHERE id = 3;
+ 
+ ALTER TABLE listt MODIFY COLUMN price INT;
 SELECT * FROM listt;
